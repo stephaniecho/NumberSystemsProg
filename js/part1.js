1d7b83ea45b2d935db061bd1370cac15011834bc
Stephanie <ChoFamilia@Hannys-MacBook-Air.local>
2020-03-23 14:41:23 -0700

    final

--- js/part1.js
+++ js/part1.js
@@ -1,14 +1,21 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedInt = document.getElementById("1_UnsignedInt").value;
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
-
-
-
-  var outputValue = "11111111";
-
+    var outputValue = null;
+    var num = 0;
+    var answer = "";
+    for (var i = 0; i < UnsignedInt.length; i++){
+      num = num * UnsignedIntBaseFrom;
+      num = num + parseInt(UnsignedInt.charAt(i));
+    }
+    console.log(num);
+    while (num > 0 ){
+      answer = (num % UnsignedIntBaseTo) + answer;
+      num = Math.floor(num / UnsignedIntBaseTo);
+    }
+    var outputValue = Number(answer);
   // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
-
+FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
 }
--- js/part2.js
+++ js/part2.js
@@ -2,11 +2,18 @@ function ConversionPart2() {
     //
     var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
 
-
+    while (SignedDecimalInt != 0 ){
+     var baseTwoNum = SignedDecimalInt % 2 + baseTwoNum;
+     SignedDecimalInt = SignedDecimalInt /2;
+   }
+   outputValue = SignedDecimalInt;
+    //     integerValue = integerValue / UnsignedIntBaseTo
+    // }
 
     var outputValue = "010110101000110110011101";
     var outputValueTwosComplement = "101001010111001001100011";
 
     // Show the output on the screen
     FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
+    return outputValue;
 }
